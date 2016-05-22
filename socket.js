63990edb01b2115be061a8f60f55bc80bd620711
Jeff Zhong <jz.liberation@gmail.com>
2016-05-22 12:50:03 -0700

    Made it so that clicking on button will show the buttons for each letter, hovering over will input that letter.

--- socket.js
+++ socket.js
@@ -24,8 +24,64 @@ window.onload = function () {
 function send(data) {
 	if(data == "A") {
 		console.log("A was printed");
-		socket.send("65");
+		//socket.send("65");
 	}
+	else if(data == "a") {
+		var result = document.getElementById("maininput").value;;
+		result += "a";
+		document.getElementById("maininput").value = result;
+		document.getElementById("abc").className = "btn btn-default animated fadeIn";
+		document.activeElement.blur();
+		document.getElementById("a").style.display = "none";
+		document.getElementById("b").style.display = "none";
+		document.getElementById("c").style.display = "none";
+		//socket.send("65");
+	}
+	else if(data == "b") {
+		var result = document.getElementById("maininput").value;;
+		result += "b";
+		document.getElementById("maininput").value = result;
+		document.getElementById("abc").className = "btn btn-default animated fadeIn";
+		document.activeElement.blur();
+		document.getElementById("a").style.display = "none";
+		document.getElementById("b").style.display = "none";
+		document.getElementById("c").style.display = "none";
+		//socket.send("65");
+	}
+	else if(data == "c") {
+		var result = document.getElementById("maininput").value;;
+		result += "c";
+		document.getElementById("maininput").value = result;
+		document.getElementById("abc").className = "btn btn-default animated fadeIn";
+		document.activeElement.blur();
+		document.getElementById("a").style.display = "none";
+		document.getElementById("b").style.display = "none";
+		document.getElementById("c").style.display = "none";
+
+		//socket.send("65");
+	}
+}
+
+function showSecond(data) {
+	if(data == "abc") {
+		if(document.getElementById(data).className.indexOf("active") != -1) {
+			document.getElementById(data).className = "btn btn-default animated fadeIn";
+			document.getElementById("copytext").focus();
+			document.getElementById("a").style.display = "none";
+			document.getElementById("b").style.display = "none";
+			document.getElementById("c").style.display = "none";
+		} else {
+			document.getElementById(data).className = "btn btn-default animated fadeIn active";
+			document.getElementById("a").style.display = "inline";
+			document.getElementById("b").style.display = "inline";
+			document.getElementById("c").style.display = "inline";
+		}
+		
+	}
+}
+
+function loseFocus() {
+	document.activeElement.blur();
 }
 
 //Sending keystroke:::: socket.send(keyCode.toString());
